9c40591ba0b521677c0d4f87d144b41020c78328
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-09-07 18:31:27 +0200

    Howto move from CRUD to Event Sourcing

--- docs-source/docs/modules/how-to/examples/shopping-cart-service-scala/src/main/scala/shopping/cart/ShoppingCart.scala
+++ docs-source/docs/modules/how-to/examples/shopping-cart-service-scala/src/main/scala/shopping/cart/ShoppingCart.scala
@@ -10,11 +10,18 @@ import akka.cluster.sharding.typed.scaladsl.EntityTypeKey
 
 object ShoppingCart {
 
+  // tag::howto-crud-to-es-initial[]
   sealed trait Command
+  final case class AddItem(itemId: String, quantity: Int) extends Command
+  final case class Checkout() extends Command
 
   sealed trait Event {
     def cartId: String
   }
+  final case class ItemAdded(cartId: String, itemId: String, quantity: Int) extends Event
+  final case class CheckedOut(cartId: String, eventTime: Instant) extends Event
+  // end::howto-crud-to-es-initial[]
+
   val EntityKey: EntityTypeKey[Command] = ???
   def apply(cartId: String, projectionTag: String): Behavior[Command] = ???
   val tags: Seq[String] = ???
