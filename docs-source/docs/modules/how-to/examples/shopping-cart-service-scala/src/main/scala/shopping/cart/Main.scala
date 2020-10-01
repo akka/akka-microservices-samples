284a2b1ff9a34534543573153c13c7db9147819b
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-10-01 18:29:07 +0200

    Prefer includes over snippets

---
+++ docs-source/docs/modules/how-to/examples/shopping-cart-service-scala/src/main/scala/shopping/cart/Main.scala
@@ -0,0 +1,13 @@
+package shopping.cart
+
+import akka.actor.typed.{ ActorSystem, Behavior }
+import akka.actor.typed.scaladsl.{ AbstractBehavior, ActorContext }
+import akka.management.scaladsl.AkkaManagement
+
+
+class Main(context: ActorContext[Nothing]) {
+  val system = context.system
+  // tag::start-akka-management[]
+  AkkaManagement(system).start()
+  // end::start-akka-management[]
+}
