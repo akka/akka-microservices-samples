7173c438e30b8a8ce24c9e12debad34dc4916436
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-02-18 11:01:14 +0100

    missing import

--- docs-source/docs/modules/microservices-tutorial/examples/00-shopping-order-service-java/src/main/java/shopping/order/Main.java
+++ docs-source/docs/modules/microservices-tutorial/examples/00-shopping-order-service-java/src/main/java/shopping/order/Main.java
@@ -4,6 +4,8 @@ import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.management.cluster.bootstrap.ClusterBootstrap;
 import akka.management.javadsl.AkkaManagement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
@@ -12,15 +14,15 @@ public class Main {
   public static void main(String[] args) throws Exception {
     ActorSystem<Void> system = ActorSystem.create(Behaviors.empty(), "ShoppingOrderService");
     try {
-    init(system);
-  } catch (Exception e) {
-    logger.error("Terminating due to initialization failure.", e);
-    system.terminate();
-  }
+      init(system);
+    } catch (Exception e) {
+      logger.error("Terminating due to initialization failure.", e);
+      system.terminate();
+    }
   }
 
   public static void init(ActorSystem<Void> system) {
-      AkkaManagement.get(system).start();
-      ClusterBootstrap.get(system).start();
+    AkkaManagement.get(system).start();
+    ClusterBootstrap.get(system).start();
   }
 }
